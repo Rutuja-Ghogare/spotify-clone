5bdf2153211145564f9c438772b41a71513f4637
ruutujaa <ghogare.r@northeastern.edu>
2024-05-21 00:27:22 -0400

    created a spotify clone using NextJS

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "lorxtxwijxnykfwbztjm.supabase.co"
+        ]
+    }
+};
+
+export default nextConfig;
